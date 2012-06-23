946d5a63bbd344ba5e3b3464669143e2ae2ec729
RKdevID <rknotid@gmail.com>
2012-06-23 14:39:49 +0900

    くりあ

--- books/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Books::Application.routes.draw do
-
-  match 'hello' => 'booklist#hello'
-  match 'bye' => 'booklist#bye'
-
-end
--- test.rb
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-require 'twitter'
-Twitter.configure do |config|
-  config.consumer_key = '8Db6qVzhZgFFA7TCJB8bw'
-  config.consumer_secret = 'SGKrXLoqF5GXjkqyf1CIb852KXPyN4O2hQJVCBRzlY'
-  config.oauth_token = '608180778-F6KZ4LIbAmLZXrrYVtiNfoaMGPPx3Y6zBKwZBwsX'
-  config.oauth_token_secret = 'kn2sWcWz9uDH4w5LRfnZXOYUHnyPufVbAkps0UOs9s'
-end 
-Twitter.update(open('twitter.txt').readlines.shuffle.first)
